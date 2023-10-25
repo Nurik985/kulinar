a4c2365f3484c827588475e2c8e6ae895b759359
Nurik985 <onurik1985@gmail.com>
2023-10-25 09:15:45 +0500

    fix paginate css

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -82,7 +82,7 @@
                 @if (is_string($element))
                 <li aria-disabled="true">
                     <button
-                        class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 cursor-default leading-5">{{
+                        class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{{
                         $element }}</button>
                 </li>
                 @endif
